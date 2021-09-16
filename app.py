07524168eaf0865742518af9795820ba47e39aa5
Fayas Noushad <TheFayasNoushad@gmail.com>
2021-09-16 18:40:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,6 +16,12 @@ def main():
     return "Documentation:- <a href='https://github.com/FayasNoushad/Play-Store-API'>Play-Store-API</a>"
 
 
+@app.route("/details/", methods=['GET'])
+def app_details():
+    query = request.args.get('query')
+    return jsonify(play_scraper.details(query))
+
+
 if __name__ == '__main__':
     app.debug = True
     app.run(host="0.0.0.0", port=5000, use_reloader=True, threaded=True)
